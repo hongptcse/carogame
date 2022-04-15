c0df081b41e86e5c0db2a83ad864bacfb2037840
hongpt.cse@gmail.com <hongpt.cse@gmail.com>
2022-04-15 16:57:38 +0700

    cap nhat

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const formatMessage = require('./resources/util/message');
 
 
 const app = express();
-const port = 443;
+const port = 80;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
